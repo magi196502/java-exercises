5c384a932bb2de5a2c557c7a0427aecc8988f018
magi196502 <macyg196502@yahoo.com>
2018-09-15 11:51:24 -0500

    Initial commit of IntegerArray

---
+++ src/exercises/IntegerArray.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class IntegerArray {
+
+    public static void main(String[] args) {
+        int numbers[] = {1, 1, 2, 3, 5, 8};
+
+        for (int num : numbers){
+            System.out.println(num);
+        }
+    }
+}
